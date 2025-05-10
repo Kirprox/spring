6d5661da24c92fd81b093dc908e6b128ca006d2d
Kir <kirpro95@yandex.ru>
2025-05-10 13:41:27 +0300

    configuration with annotations+xml+autowired

---
+++ src/main/java/com/zaurtregulov/spring/spring_introduction/configs/ConfigWithAnnotations1.java
@@ -0,0 +1,19 @@
+package com.zaurtregulov.spring.spring_introduction.configs;
+
+import com.zaurtregulov.spring.spring_introduction.Cat;
+import com.zaurtregulov.spring.spring_introduction.Person;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigWithAnnotations1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext3.xml");
+
+//        Cat mycat = context.getBean("cat", Cat.class);
+//        mycat.say();
+
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+        context.close();
+    }
+}
