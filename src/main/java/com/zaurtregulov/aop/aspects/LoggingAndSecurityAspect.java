c7491f9fd303e7ce341c7379adb89ac125f581ef
kir <kirpro95@yandex.ru>
2025-05-17 16:59:33 +0300

    комбинирование pointcut + объявление+samples2

--- src/main/java/com/zaurtregulov/aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/com/zaurtregulov/aop/aspects/LoggingAndSecurityAspect.java
@@ -9,38 +9,58 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAndSecurityAspect {
 
+    @Pointcut("execution(* com.zaurtregulov.aop.UniLibrary.*(..))")
+    private void allMethodsFromUniLibrary() {
 
-
-    @Before("execution(* getBook(..))")
-    public void beforeAnyGetBookMethod() {
-        System.out.println("✅ AOP работает: метод getBook вызван");
-    }
-    @Pointcut("execution(* com.zaurtregulov.aop.UniLibrary.get*())")
-    private void allGetMethodsFromUniLibrary() {
     }
 
-    @Pointcut("execution(* com.zaurtregulov.aop.UniLibrary.return*())")
-    private void allReturnMethodsFromUniLibrary() {
+    @Pointcut("execution(public void com.zaurtregulov.aop.UniLibrary.returnMagazine())")
+    private void returnMagazinFromUniLibrary() {
+
+
     }
 
-    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
-    private void allGetAndReturnMethodsFromUniLibrary() { // ← ИМЯ метода тут важно!
+    @Pointcut("allMethodsFromUniLibrary() && !returnMagazinFromUniLibrary()")
+    private void allMethodsExceptReturnMagazineFromUniLibrary() {
+
     }
 
-    @Before("allGetMethodsFromUniLibrary()")
-    public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetLoggingAdvice()Log 1");
+    @Before("allMethodsExceptReturnMagazineFromUniLibrary()")
+    public void beforeAllMethodsExceptReturnMagazineAdvice() {
+        System.out.println("allMethodsExceptReturnMagazineFromUniLibrary() log 10");
     }
 
-    @Before("allReturnMethodsFromUniLibrary()")
-    public void beforeReturnLoggingAdvice() {
-        System.out.println("beforeReturnLoggingAdvice() Log 2");
-    }
 
-    @Before("allGetAndReturnMethodsFromUniLibrary()")
-    public void beforeGetAndReturnLoggingAdvice() {
-        System.out.println("beforeGetAndReturnLoggingAdvice Log 3");
-    }
+//    @Before("execution(* getBook(..))")
+//    public void beforeAnyGetBookMethod() {
+//        System.out.println("AOP работает: метод getBook вызван");
+//    }
+//    @Pointcut("execution(* com.zaurtregulov.aop.UniLibrary.get*())")
+//    private void allGetMethodsFromUniLibrary() {
+//    }
+//
+//    @Pointcut("execution(* com.zaurtregulov.aop.UniLibrary.return*())")
+//    private void allReturnMethodsFromUniLibrary() {
+//    }
+//
+//    @Pointcut("allGetMethodsFromUniLibrary() || allReturnMethodsFromUniLibrary()")
+//    private void allGetAndReturnMethodsFromUniLibrary() { // ← ИМЯ метода тут важно!
+//    }
+//
+//    @Before("allGetMethodsFromUniLibrary()")
+//    public void beforeGetLoggingAdvice() {
+//        System.out.println("beforeGetLoggingAdvice()Log 1");
+//    }
+//
+//    @Before("allReturnMethodsFromUniLibrary()")
+//    public void beforeReturnLoggingAdvice() {
+//        System.out.println("beforeReturnLoggingAdvice() Log 2");
+//    }
+//
+//    @Before("allGetAndReturnMethodsFromUniLibrary()")
+//    public void beforeGetAndReturnLoggingAdvice() {
+//        System.out.println("beforeGetAndReturnLoggingAdvice Log 3");
+//    }
 
 
 //    @Pointcut("execution(* get*())")
