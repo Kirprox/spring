c6a96cb12ef7b6eae6e5f721ed80f929552c2783
kir <kirpro95@yandex.ru>
2025-05-21 09:41:24 +0300

    получение объектов из бд

--- src/main/java/com/zaurtregulov/hibernate_test/tests/Test2.java
+++ src/main/java/com/zaurtregulov/hibernate_test/tests/Test2.java
@@ -5,21 +5,25 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
-                 .configure("hibernate.cfg.xml")
-                 .addAnnotatedClass(Employee.class)
-                 .buildSessionFactory();
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
         try {
             Session session = factory.getCurrentSession();
             Employee emp = new Employee(
-                    "Alexander","Ivanov", "IT", 600);
+                    "Oleg", "Sidorov", "HR", 700);
             session.beginTransaction();
             session.save(emp);
+
+            int myId = emp.getId();
+
+            Employee employee = session.get(Employee.class, myId);
             session.getTransaction().commit();
+            System.out.println(employee);
             System.out.println("Done!");
-            System.out.println(emp);
         } finally {
             factory.close();
         }
--- src/main/java/com/zaurtregulov/hibernate_test/tests/Test3.java
+++ src/main/java/com/zaurtregulov/hibernate_test/tests/Test3.java
@@ -5,21 +5,29 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+import java.util.List;
+
+public class Test3 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
-                 .configure("hibernate.cfg.xml")
-                 .addAnnotatedClass(Employee.class)
-                 .buildSessionFactory();
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
         try {
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee(
-                    "Alexander","Ivanov", "IT", 600);
             session.beginTransaction();
-            session.save(emp);
+//            List<Employee> emps = session.createQuery("from Employee")
+//                    .getResultList();
+            List<Employee> emps = session.createQuery("from Employee " +
+                            "where name = 'Alexander' AND salary > 650")
+                    .getResultList();
+
+            for (Employee e : emps) {
+                System.out.println(e);
+            }
+
             session.getTransaction().commit();
             System.out.println("Done!");
-            System.out.println(emp);
         } finally {
             factory.close();
         }
