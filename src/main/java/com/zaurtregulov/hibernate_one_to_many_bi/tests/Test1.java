d9f27120767f0e6fc273ce193a1e149190d8babd
Kir <kirpro95@yandex.ru>
2025-05-22 23:15:27 +0300

    types of loading(Eager,lazy)

--- src/main/java/com/zaurtregulov/hibernate_one_to_many_bi/tests/Test1.java
+++ src/main/java/com/zaurtregulov/hibernate_one_to_many_bi/tests/Test1.java
@@ -17,38 +17,51 @@ public class Test1 {
         try {
             //добавление
 //            session = factory.getCurrentSession();
-//            Department dep = new Department("IT",300,1200);
-//            Employee emp1 = new Employee("Zaur","Tregulov", 800);
-//            Employee emp2 = new Employee("Elena","Smirnova", 1000);
-//            dep.addEmployeeToDepartment(emp1);
-//            dep.addEmployeeToDepartment(emp2);
-//
-//            session.beginTransaction();
-//            session.persist(dep);
-//
-//            session.getTransaction().commit();
-//            System.out.println("Done!");
-
-
-//            session = factory.getCurrentSession();
-//
-//            session.beginTransaction();
-//            Employee employee = session.get(Employee.class, 1);
-//            System.out.println(employee);
-//            System.out.println(employee.getDepartment());
-//
-//            session.getTransaction().commit();
-//            System.out.println("Done!");
-
-            session = factory.getCurrentSession();/////удаление
+////            Department dep = new Department("sale",800,1500);
+////            Employee emp1 = new Employee("Zaur","Tregulov", 800);
+////            Employee emp2 = new Employee("Elena","Smirnova", 1500);
+////            Employee emp3 = new Employee("Anton","Sidorov", 1200);
+////            dep.addEmployeeToDepartment(emp1);
+////            dep.addEmployeeToDepartment(emp2);
+////            dep.addEmployeeToDepartment(emp3);
+////
+////            session.beginTransaction();
+////            session.persist(dep);
+////
+////            session.getTransaction().commit();
+////            System.out.println("Done!");
+// *********************************************************************
+//чтение
+            session = factory.getCurrentSession();
 
             session.beginTransaction();
-            Employee employee = session.get(Employee.class, 4);
-            session.remove(employee);
+            System.out.println("Get department");
+            Department department = session.get(Department.class, 6);
+
+            System.out.println("show department");
+            System.out.println(department);
+
+            System.out.println("подгрузим роботников");
+            department.getEmps().get(0);
             session.getTransaction().commit();
+            System.out.println("show employees of the department");
+            System.out.println(department.getEmps());
             System.out.println("Done!");
+// *********************************************************************
+//удаление
+//            session = factory.getCurrentSession();/////удаление
+//
+//            session.beginTransaction();
+//            Employee employee = session.get(Employee.class, 4);
+//            session.remove(employee);
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+            //типы загрузок
+
+//
 
         } finally {
+            session.close();
             factory.close();
         }
     }
